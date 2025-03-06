30b52edbb4474c0c433ede1c1729119d4f654b51
LKenji <54600289+lucaskmk@users.noreply.github.com>
2025-03-06 08:40:19 -0300

    Teste4

--- editora/settings.py
+++ editora/settings.py
@@ -23,8 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-&6!60n5-(f%ws_)f=q33*!(+kau&e_n#t@tjn16k_)jqww9kh5'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# SECURITY WARNING: don't run with debug turned on in production! ---------------------------
+
+#==================================
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
